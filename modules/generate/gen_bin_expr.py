f7f06c0298b3655809359a71e3e04d419cf09e9b
RetroTho <74274119+RetroTho@users.noreply.github.com>
2023-11-21 12:41:14 -0800

    Refactor generator

--- modules/generate/gen_bin_expr.py
+++ modules/generate/gen_bin_expr.py
@@ -3,29 +3,33 @@ from modules.generate.common import *
 
 
 class GenBinExpr:
+    def _matchBinExpr(
+        self,
+        vari: (
+            NodeBinaryExprIsEq
+            | NodeBinaryExprAdd
+            | NodeBinaryExprSub
+            | NodeBinaryExprMult
+            | NodeBinaryExprDiv
+        ),
+    ) -> str:
+        match vari:
+            case NodeBinaryExprIsEq():
+                return "=="
+            case NodeBinaryExprAdd():
+                return "+"
+            case NodeBinaryExprSub():
+                return "-"
+            case NodeBinaryExprMult():
+                return "*"
+            case NodeBinaryExprDiv():
+                return "/"
+            case _:
+                print("Generating Error: invalid binary operation")
+                exit()
+
     def generate(self, binary_expr: NodeBinaryExpr):
-        if isinstance(binary_expr.vari, NodeBinaryExprIsEq):
-            is_eq = binary_expr.vari
-            GenCalls.genExpr(is_eq.left)
-            GenVars.buffer += "=="
-            GenCalls.genExpr(is_eq.right)
-        elif isinstance(binary_expr.vari, NodeBinaryExprAdd):
-            add = binary_expr.vari
-            GenCalls.genExpr(add.left)
-            GenVars.buffer += "+"
-            GenCalls.genExpr(add.right)
-        elif isinstance(binary_expr.vari, NodeBinaryExprSub):
-            sub = binary_expr.vari
-            GenCalls.genExpr(sub.left)
-            GenVars.buffer += "-"
-            GenCalls.genExpr(sub.right)
-        elif isinstance(binary_expr.vari, NodeBinaryExprMult):
-            mult = binary_expr.vari
-            GenCalls.genExpr(mult.left)
-            GenVars.buffer += "*"
-            GenCalls.genExpr(mult.right)
-        elif isinstance(binary_expr.vari, NodeBinaryExprDiv):
-            div = binary_expr.vari
-            GenCalls.genExpr(div.left)
-            GenVars.buffer += "/"
-            GenCalls.genExpr(div.right)
+        bin_expr_vari = binary_expr.vari
+        GenCalls.genExpr(bin_expr_vari.left)
+        GenVars.buffer += self._matchBinExpr(bin_expr_vari)
+        GenCalls.genExpr(bin_expr_vari.right)
